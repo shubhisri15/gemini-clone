501d2d866a31f248cd37b09f84ae3a48a524c8d4
Shubhi Srivastava <91206957+shubhisri15@users.noreply.github.com>
2025-10-03 19:38:35 +0530

    dashboard scaffolding

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const UserDashboard = () => {
   return (
-    <div>UserDashboard</div>
+    <div className='w-screen flex flex-col items-center gap-8'>
+        <h1 className='font-bold text-3xl'>What's on your mind today?</h1>
+        <input type='text' placeholder='Type something' className='w-1/2 border-1 border-white rounded-xl py-4 px-2'></input>
+    </div>
   )
 }
 
